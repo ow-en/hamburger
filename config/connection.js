75450f2b23da7840dae69dce7ab3e3fdd1700ada
owensource <ciruz001@cougars.csusm.edu>
2018-02-25 14:28:05 -0800

    fixing sql

--- config/connection.js
+++ config/connection.js
@@ -6,20 +6,21 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        port: 3306,
-        host: 'localhost',
-        user: "root",
-        password: '',
-        database: 'burgers_db'
+            port: 3306,
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'burgers_db'
     })
+
 };
 
 connection.connect(function(err) {
-    if(err) {
-        console.log('ERROR: mysql connection error ' + err.stack + '\n\n');
+    if (err) {
+        console.log('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
         return;
     }
-    console.log('Connected as id ' + connection.threadId + '\n\n');
+    console.log('Connected as MySQL database as id ' + connection.threadId + '\n\n');
 });
 
 module.exports = connection;
